23385894574735c33324ef97d33263ca7a4321fd
ConorGover <98365021+ConorGover@users.noreply.github.com>
2024-02-21 07:27:56 -0500

    improved cellect

--- cellect.py
+++ cellect.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 CELL_DATA_PATH = 'G7_2023.csv'
 MODULES = 35
@@ -25,40 +26,123 @@ def process_cells(cells):
     avg_st = np.mean(cells['st'])
     dev_st = np.abs(cells['st'] - avg_st) / avg_st
 
-    largest_dev = np.where(dev_st > dev_lt, 'st', 'lt')
-    cells = np.array([(cell['num'], cell['v0'], cell['st'], cell['lt'], dev_st[i], dev_lt[i], max(dev_lt[i], dev_st[i]), largest_dev[i], 0) for i, cell in enumerate(cells)], dtype=[('num', 'i8'), ('v0', 'f8'), ('st', 'f8'), ('lt', 'f8'), ('dev_st', 'f8'), ('dev_lt', 'f8'), ('dev', 'f8'), ('largest_dev', 'U2'), ('mod', 'i8')])
-    cells = np.sort(cells, order='dev')
+    # remove cells with dev_st > 1/2 standard deviation
+    std_dev_st = np.std(dev_st)
+    st_mean = np.mean(dev_st)
+    bad_cells = cells[dev_st >= st_mean + 0.5 * std_dev_st]
+    cells = cells[dev_st < st_mean + 0.5 * std_dev_st]
 
-    return cells
+    dev_st = np.abs(cells['st'] - avg_st) / avg_st
+    dev_lt = np.abs(cells['lt'] - fit(cells['v0'])) / fit(cells['v0'])
+    largest_dev = np.where(dev_st > dev_lt, 'st', 'lt')
+
+    bc_dev_st = np.abs(bad_cells['st'] - avg_st) / avg_st
+    bc_dev_lt = np.abs(bad_cells['lt'] - fit(bad_cells['v0'])) / fit(bad_cells['v0'])
+    bc_largest_dev = np.where(bc_dev_st > bc_dev_lt, 'st', 'lt')
+    
+    print('Number of cells removed:', len(bad_cells))
+    print('Number of cells remaining:', len(cells))
+
+    # plot the distribution of dev_st
+    # plt.hist(dev_st, bins=20)
+    # plt.xlabel('Deviation from mean')
+    # plt.ylabel('Number of cells')
+    # plt.show()
+
+    median_lt = np.median(cells['lt'])
+    cells = np.array([(cell['num'], cell['v0'], cell['st'], cell['lt'], dev_st[i], dev_lt[i], max(dev_lt[i], dev_st[i]), largest_dev[i], cell['lt'] - median_lt, 0) for i, cell in enumerate(cells)], dtype=[('num', 'i8'), ('v0', 'f8'), ('st', 'f8'), ('lt', 'f8'), ('dev_st', 'f8'), ('dev_lt', 'f8'), ('dev', 'f8'), ('largest_dev', 'U2'), ('dist', 'f8'), ('mod', 'i8')])
+    bad_cells = np.array([(cell['num'], cell['v0'], cell['st'], cell['lt'], bc_dev_st[i], bc_dev_lt[i], max(bc_dev_lt[i], bc_dev_st[i]), bc_largest_dev[i], cell['lt'] - median_lt, 0) for i, cell in enumerate(bad_cells)], dtype=[('num', 'i8'), ('v0', 'f8'), ('st', 'f8'), ('lt', 'f8'), ('dev_st', 'f8'), ('dev_lt', 'f8'), ('dev', 'f8'), ('largest_dev', 'U2'), ('dist', 'f8'), ('mod', 'i8')])
+    
+    return cells, bad_cells
+
+def assign_to_modules(cells, starting_module=1):
+    cells = np.sort(cells, order='dist')
+    total = len(cells)
+    # find the index of the cell with dist closest to 0
+    median_index = np.argmin(np.abs(cells['dist']))
+    # adjust the center to be a multiple of CELLS_PER_MODULE so we can match as many cells as possible
+    dist_start_to_middle = median_index - CELLS_PER_MODULE // 2
+    dist_middle_to_end = total - median_index + CELLS_PER_MODULE // 2
+    remaining_end = (total - dist_middle_to_end) % CELLS_PER_MODULE
+    remaining_start = dist_start_to_middle % CELLS_PER_MODULE
+    if remaining_start <= remaining_end:
+        center = median_index - remaining_start
+    else:
+        center = median_index + remaining_end
+
+    mod = 1
+    lower = center - CELLS_PER_MODULE // 2
+    upper = center + CELLS_PER_MODULE // 2
+    up_or_down = 1
+    while lower >= 0 and upper <= total:
+        cells[lower:upper]['mod'] = starting_module + mod - 1
+        center += mod * up_or_down * CELLS_PER_MODULE
+        lower = center - CELLS_PER_MODULE // 2
+        upper = center + CELLS_PER_MODULE // 2
+        mod += 1
+        up_or_down *= -1
+
+    center += mod * up_or_down * CELLS_PER_MODULE
+
+    if lower < 0:
+        center += mod * up_or_down * CELLS_PER_MODULE
+        lower = center - CELLS_PER_MODULE // 2
+        upper = center + CELLS_PER_MODULE // 2
+        while upper <= total:
+            cells[lower:upper]['mod'] = starting_module + mod - 1
+            center += CELLS_PER_MODULE
+            lower = center - CELLS_PER_MODULE // 2
+            upper = center + CELLS_PER_MODULE // 2
+            mod += 1
+    elif upper > total:
+        upper = center + CELLS_PER_MODULE // 2
+        lower = center - CELLS_PER_MODULE // 2
+        while lower >= 0:
+            cells[lower:upper]['mod'] = starting_module + mod - 1
+            center -= CELLS_PER_MODULE
+            lower = center - CELLS_PER_MODULE // 2
+            upper = center + CELLS_PER_MODULE // 2
+            mod += 1
+
+    total_modules = mod - 1
+            
+    return cells, total_modules
 
 cells = read_cells(CELL_DATA_PATH)
-cells_sorted = process_cells(cells)
+cells, bad_cells = process_cells(cells)
+cells, total_modules = assign_to_modules(cells, 1)
 
-modNum = 1
-count = 1
-for cell in cells_sorted:
-    if count <= 12:
-        cell['mod'] = modNum
-    else:
-        modNum += 1
-        count = 1
-        cell['mod'] = modNum
-    count += 1
+# find cells that are not assigned to any module
+unused = cells[cells['mod'] == 0]
+cells = cells[cells['mod'] != 0]
+# add the bad cells to the unused list
+unused = np.concatenate((unused, bad_cells))
+unused = np.sort(unused, order='dist')
+unused = assign_to_modules(unused, total_modules + 1)[0]
 
-good_cells = cells_sorted[ :MODULES * CELLS_PER_MODULE]
-bad_cells = cells_sorted[MODULES * CELLS_PER_MODULE: ]
+cells_sorted = np.concatenate((cells, unused))
 
-np.savetxt('details.csv', cells_sorted, delimiter=',', header='num,v0,st,lt,dev_st,dev_lt,dev,largest_dev,mod', fmt='%i,%f,%f,%f,%f,%f,%f,%s,%i')
+plt.scatter(cells_sorted['mod'], cells_sorted['dist'])
+plt.xlabel('Module')
+plt.ylabel('Distance from median')
+plt.show()
+
+np.savetxt('useless_details--NOT_important--DO_NOT_READ.csv', cells_sorted, delimiter=',', header='num,v0,st,lt,dev_st,dev_lt,dev,largest_dev,dist,mod', fmt='%i,%f,%f,%f,%f,%f,%f,%s,%f,%i')
 
 with open ('module_list.txt', 'w') as file:
-    for mod in range(1, len(cells_sorted) // CELLS_PER_MODULE):
+    for mod in range(1, len(cells_sorted) // CELLS_PER_MODULE + 1):
         file.write(f'Module {mod}: ')
         cells_in_mod = [cell['num'] for cell in cells_sorted if cell['mod'] == mod]
         cells_in_mod.sort()
         file.write(', '.join(str(cell) for cell in cells_in_mod))
         file.write('\n')
+    leftovers = [cell['num'] for cell in cells_sorted if cell['mod'] == 0]
+    if len(leftovers) > 0:
+        leftovers.sort()
+        file.write('Leftovers: ')
+        file.write(', '.join(str(cell) for cell in leftovers))
 
 with open ('cell_list.txt', 'w') as file:
     cells_sorted.sort(order='num')
     for cell in cells_sorted:
-        file.write(f'Cell {cell["num"]}: module {cell["mod"]}\n')
+        file.write(f'Cell {cell["num"]}: module {cell["mod"] if cell["mod"] != 0 else 'NONE'}\n')
